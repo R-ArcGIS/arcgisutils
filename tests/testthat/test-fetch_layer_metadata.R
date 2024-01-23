1c2c94ffe35244ab1d9807ec0490fa6096a96d9d
Josiah Parry <josiah.parry@gmail.com>
2024-01-23 10:27:30 -0500

    add https

--- tests/testthat/test-fetch_layer_metadata.R
+++ tests/testthat/test-fetch_layer_metadata.R
@@ -1,5 +1,5 @@
 test_that("fetch_layer_metadata() NULL and empty string", {
-  furl <- "http://services.arcgisonline.com/arcgis/rest/services/USA_Topo_Maps/MapServer"
+  furl <- "https://services.arcgisonline.com/arcgis/rest/services/USA_Topo_Maps/MapServer"
   req <- httr2::request(furl)
 
   expect_equal(
