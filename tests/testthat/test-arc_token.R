5851e1f30b2a9d374451e32730e90d03a554d25a
Josiah Parry <josiah.parry@gmail.com>
2024-02-09 08:46:39 -0500

    update tests

--- tests/testthat/test-arc_token.R
+++ tests/testthat/test-arc_token.R
@@ -1,8 +1,7 @@
 test_that("tokens set and fetched by name are identical", {
   # create fake tokens
-  token_a <- httr2::oauth_token("1234")
-  token_b <- httr2::oauth_token("abcd")
-
+  token_a <- httr2::oauth_token("1234", arcgis_host = arc_host())
+  token_b <- httr2::oauth_token("abcd", arcgis_host = arc_host())
 
   # set token by name
   set_arc_token(org_a = token_a, org_b = token_b)
@@ -15,7 +14,7 @@ test_that("tokens set and fetched by name are identical", {
 test_that("Default token set and fetched", {
 
   # create fake token
-  token <- httr2::oauth_token("1234")
+  token <- httr2::oauth_token("1234", arcgis_host = arc_host())
 
   # set token to the default location
   set_arc_token(token)
@@ -28,9 +27,9 @@ test_that("Default token set and fetched", {
 test_that("Default token and named tokens", {
 
   # create fake tokens
-  token <- httr2::oauth_token("....")
-  token_a <- httr2::oauth_token("1234")
-  token_b <- httr2::oauth_token("abcd")
+  token <- httr2::oauth_token("....", arcgis_host = arc_host())
+  token_a <- httr2::oauth_token("1234", arcgis_host = arc_host())
+  token_b <- httr2::oauth_token("abcd", arcgis_host = arc_host())
 
 
   # set token by name and default
@@ -41,4 +40,21 @@ test_that("Default token and named tokens", {
   expect_identical(arc_token("org_a"), token_a)
   expect_identical(arc_token("org_b"), token_b)
 
+})
+
+
+test_that("Host must be set:", {
+  token <- httr2::oauth_token("....")
+  expect_error(
+    obj_check_token(token),
+    "`token` does not have \"arcgis_host\""
+  )
+})
+
+test_that("Only one host can be set:", {
+  token <- httr2::oauth_token("....", arcgis_host = c("a", "b"))
+  expect_error(
+    obj_check_token(token),
+    "`token` has more than one \"arcgis_host\""
+  )
 })
