4b7373ed66b813ada14dc0414b7e611396d89fd1
Josiah Parry <josiah.parry@gmail.com>
2025-12-02 08:56:14 -0800

    update auth with some docs

--- R/auth-shiny.R
+++ R/auth-shiny.R
@@ -1,14 +1,13 @@
-auth_shiny <- function(
-  client = Sys.getenv("ARCGIS_CLIENT"),
-  secret = Sys.getenv("ARCGIS_SECRET"),
-  host = arc_host(),
-  redirect_uri = Sys.getenv("ARCGIS_REDIRECT_URI"),
-  ...
+#' Authenticate with Shiny
+#' @export
+#' @name auth_shiny
+oauth_provider_arcgis <- function(
+  host = arc_host()
 ) {
   ver <- utils::packageVersion("arcgisutils")
   arc_ver <- paste0("arcgisutils v", ver)
 
-  provider <- oauth_provider(
+  oauth_provider(
     name = arc_ver,
     auth_url = paste0(host, "/sharing/rest/oauth2/authorize"),
     token_url = paste0(host, "/sharing/rest/oauth2/token"),
@@ -16,62 +15,28 @@ auth_shiny <- function(
     userinfo_url = paste0(host, "/sharing/rest/portals/self?f=json"),
     userinfo_id_selector = function(x) x$id
   )
-
-  shinyOAuth::oauth_client(
-    provider = provider,
-    client_id = client,
-    client_secret = secret,
-    redirect_uri = redirect_uri
-  )
 }
 
-# library(shiny)
-# library(shinyOAuth)
-# library(arcgisutils)
-# # Simple UI
-# ui <- fluidPage(
-#   use_shinyOAuth(),
-#   uiOutput("login_information")
-# )
+#' @param redirect_uri default `Sys.getenv("ARCGIS_REDIRECT_URI")`. The redirect URL after completing authentication flow.
+#' @param ... additional arguments passed to [shinyOAuth::oauth_client()]
+#' @inheritParams auth_client
+#' @export
+#' @name auth_shiny
+auth_shiny <- function(
+  client = Sys.getenv("ARCGIS_CLIENT"),
+  secret = Sys.getenv("ARCGIS_SECRET"),
+  redirect_uri = Sys.getenv("ARCGIS_REDIRECT_URI"),
+  host = arc_host(),
+  ...
+) {
+  dots <- rlang::list2(...)
+  check_dots_named(dots)
 
-# client <- auth_shiny_provider()
-# oauth_provider_arcgis()
-
-# server <- function(input, output, session) {
-
-#   # Set auto_redirect = FALSE for manual login
-#   auth <- shinyOAuth::oauth_module_server(
-#     "auth",
-#     client,
-#     auto_redirect = FALSE
-#   )
-
-#   output$login_information <- renderUI({
-#     if (auth$authenticated) {
-#       user_info <- auth$token@userinfo
-#       tagList(
-#         tags$p("You are logged in! Your details:"),
-#         tags$pre(paste(capture.output(str(user_info)), collapse = "\n"))
-#       )
-#     } else {
-#       tagList(
-#         tags$p("You are not logged in."),
-#         actionButton("login_btn", "Login with ArcGIS")
-#       )
-#     }
-#   })
-
-#   # Trigger login when button is clicked
-#   observeEvent(input$login_btn, {
-#     auth$request_login()
-#   })
-# }
-
-# runApp(
-#   shinyApp(ui, server),
-#   port = 8100,
-#   launch.browser = FALSE
-# )
-
-# # Open the app in your regular browser at http://127.0.01:8100
-# # (viewers in RStudio/Positron/etc. cannot perform necessary redirects)
+  shinyOAuth::oauth_client(
+    provider = oauth_provider_arcgis(host),
+    client_id = client,
+    client_secret = secret,
+    redirect_uri = redirect_uri,
+    !!!dots
+  )
+}
---
+++ inst/example-auth-shiny.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(shinyOAuth)
+library(arcgisutils)
+# Simple UI
+ui <- fluidPage(
+  use_shinyOAuth(),
+  uiOutput("login_information")
+)
+
+client <- auth_shiny_provider()
+oauth_provider_arcgis()
+
+server <- function(input, output, session) {
+  # Set auto_redirect = FALSE for manual login
+  auth <- shinyOAuth::oauth_module_server(
+    "auth",
+    client,
+    auto_redirect = FALSE
+  )
+
+  output$login_information <- renderUI({
+    if (auth$authenticated) {
+      user_info <- auth$token@userinfo
+      tagList(
+        tags$p("You are logged in! Your details:"),
+        tags$pre(paste(capture.output(str(user_info)), collapse = "\n"))
+      )
+    } else {
+      tagList(
+        tags$p("You are not logged in."),
+        actionButton("login_btn", "Login with ArcGIS")
+      )
+    }
+  })
+
+  # Trigger login when button is clicked
+  observeEvent(input$login_btn, {
+    auth$request_login()
+  })
+}
+
+runApp(
+  shinyApp(ui, server),
+  port = 8100,
+  launch.browser = FALSE
+)
+
+# Open the app in your regular browser at http://127.0.01:8100
+# (viewers in RStudio/Positron/etc. cannot perform necessary redirects)
